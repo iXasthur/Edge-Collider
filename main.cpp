c4cf6a67acab7a4992762270f925a5376ff72727
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2020-09-14 00:15:22 +0300

    Created window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,84 @@
-#include <iostream>
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+#ifndef UNICODE
+#define UNICODE
+#endif
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <iostream>
+#include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow) {
+    const wchar_t WINDOW_CLASS[] = L"MAIN_WINDOW_CLASS";
+    const wchar_t WINDOW_TITLE[] = L"Edge collider ^_^";
+
+    WNDCLASSEXW wc;
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
+    wc.lpszMenuName = nullptr;
+    wc.lpszClassName = WINDOW_CLASS;
+    wc.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
+
+    RegisterClassExW(&wc);
+
+    HWND hwnd = CreateWindowExW(
+            0,                              // Optional window styles.
+            WINDOW_CLASS,                     // Window class
+            WINDOW_TITLE,    // Window text
+            WS_OVERLAPPEDWINDOW,            // Window style
+
+            // Size and position
+            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+            nullptr,       // Parent window
+            nullptr,       // Menu
+            hInstance,  // Instance handle
+            nullptr        // Additional application data
+    );
+
+    if (hwnd == nullptr) {
+        std::cout << "1";
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    MSG msg = {};
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_DESTROY: {
+            PostQuitMessage(0);
+            break;
+        }
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW + 1));
+
+            EndPaint(hwnd, &ps);
+            break;
+        }
+        default: {
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+        }
+    }
     return 0;
 }
