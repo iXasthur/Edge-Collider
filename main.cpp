e98468fbb84c2d049c0e313d5b8f0a8a4ed87119
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2020-09-14 16:46:38 +0300

    Created rectangle

--- main.cpp
+++ main.cpp
@@ -8,9 +8,22 @@
 #include <iostream>
 #include <windows.h>
 
+
+const SIZE MIN_WINDOW_SIZE = SIZE {400, 300};
+const SIZE FIRST_WINDOW_SIZE = SIZE {800, 600};
+const SIZE MOVING_RECT_SIZE = SIZE {50, 50};
+
+const COLORREF BACKGROUND_COLOR = RGB(26, 26, 26);
+const COLORREF MOVING_RECT_COLOR = RGB(241, 196, 15);
+
+static POINT movingRectPosition = POINT {25, 25};
+
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow) {
+
     const wchar_t WINDOW_CLASS[] = L"MAIN_WINDOW_CLASS";
     const wchar_t WINDOW_TITLE[] = L"Edge collider ^_^";
 
@@ -21,41 +34,39 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hInstance = hInstance;
-    wc.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
-    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wc.hIcon = LoadIconW(nullptr, IDI_APPLICATION);
+    wc.hCursor = LoadCursorW(nullptr, IDC_ARROW);
     wc.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
     wc.lpszMenuName = nullptr;
     wc.lpszClassName = WINDOW_CLASS;
-    wc.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
+    wc.hIconSm = LoadIconW(nullptr, IDI_APPLICATION);
 
     RegisterClassExW(&wc);
 
     HWND hwnd = CreateWindowExW(
-            0,                              // Optional window styles.
-            WINDOW_CLASS,                     // Window class
-            WINDOW_TITLE,    // Window text
-            WS_OVERLAPPEDWINDOW,            // Window style
-
+            0x0,  // Optional window styles.
+            WINDOW_CLASS,   // Window class
+            WINDOW_TITLE,   // Window text
+            WS_OVERLAPPEDWINDOW,   // Window style
             // Size and position
-            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
-            nullptr,       // Parent window
-            nullptr,       // Menu
+            CW_USEDEFAULT, CW_USEDEFAULT, FIRST_WINDOW_SIZE.cx, FIRST_WINDOW_SIZE.cy,
+            nullptr,   // Parent window
+            nullptr,   // Menu
             hInstance,  // Instance handle
-            nullptr        // Additional application data
+            nullptr   // Additional application data
     );
 
     if (hwnd == nullptr) {
-        std::cout << "1";
+        MessageBoxW(nullptr, L"Error creating window!", L"Attention", MB_OK);
         return 0;
     }
 
     ShowWindow(hwnd, nCmdShow);
 
     MSG msg = {};
-    while (GetMessage(&msg, nullptr, 0, 0)) {
+    while (GetMessageW(&msg, nullptr, 0, 0)) {
         TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        DispatchMessageW(&msg);
     }
 
     return 0;
@@ -71,9 +82,34 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hwnd, &ps);
 
-            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW + 1));
+            RECT rect;
+            GetClientRect(hwnd, &rect);
+
+            HBRUSH backgroundBrush = CreateSolidBrush(BACKGROUND_COLOR);
+            FillRect(hdc, &ps.rcPaint, backgroundBrush);
+
+            SelectObject(hdc, GetStockObject(DC_PEN));
+            SelectObject(hdc, GetStockObject(DC_BRUSH));
+
+            COLORREF rectColor = MOVING_RECT_COLOR;
+            SetDCPenColor(hdc, rectColor);
+            SetDCBrushColor(hdc, rectColor);
+
+            RECT movingRect;
+            movingRect.left = 0 + movingRectPosition.x;
+            movingRect.top = 0 + movingRectPosition.y;
+            movingRect.right = MOVING_RECT_SIZE.cx + movingRectPosition.x;
+            movingRect.bottom = MOVING_RECT_SIZE.cy + movingRectPosition.y;
+
+            Rectangle(hdc, movingRect.left, movingRect.top, movingRect.right, movingRect.bottom);
 
             EndPaint(hwnd, &ps);
+            break;
+        }
+        case WM_GETMINMAXINFO: {
+            LPMINMAXINFO lpMMI = (LPMINMAXINFO) lParam;
+            lpMMI->ptMinTrackSize.x = MIN_WINDOW_SIZE.cx;
+            lpMMI->ptMinTrackSize.y = MIN_WINDOW_SIZE.cy;
             break;
         }
         default: {
